2182ab655816bad27a1e1cd842c1790d70646829
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2018-05-25 10:48:01 +0300

    Add Geohash

--- geoposition/__init__.py
+++ geoposition/__init__.py
@@ -1,14 +1,18 @@
 from __future__ import unicode_literals
 from decimal import Decimal
+from .geohash import geo_encode
+
 
 default_app_config = 'geoposition.apps.GeoPositionConfig'
 
 VERSION = (0, 3, 0)
 __version__ = '.'.join(map(str, VERSION))
 
+GEOHASH_PRECISION = 9
+
 
 class Geoposition(object):
-    def __init__(self, latitude, longitude):
+    def __init__(self, latitude, longitude, geohash=None):
         if isinstance(latitude, float) or isinstance(latitude, int):
             latitude = str(latitude)
         if isinstance(longitude, float) or isinstance(longitude, int):
@@ -16,6 +20,11 @@ class Geoposition(object):
 
         self.latitude = Decimal(latitude)
         self.longitude = Decimal(longitude)
+
+        if geohash is None:
+            self.rehash()
+        else:
+            self.geohash = geohash
 
     def __str__(self):
         return "%s,%s" % (self.latitude, self.longitude)
@@ -31,3 +40,14 @@ class Geoposition(object):
 
     def __ne__(self, other):
         return not isinstance(other, Geoposition) or self.latitude != other.latitude or self.longitude != other.longitude
+
+    def __gt__(self, other):
+        return not isinstance(other, Geoposition) or self.geohash > other.geohash
+
+    def __lt__(self, other):
+        return not isinstance(other, Geoposition) or self.geohash < other.geohash
+
+    def rehash(self):
+        self.geohash = geo_encode(
+            float(self.latitude), float(self.longitude), GEOHASH_PRECISION
+        )
--- geoposition/fields.py
+++ geoposition/fields.py
@@ -1,12 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.six import with_metaclass
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import smart_text
+from django.db.models import Lookup
+from django.db.models.lookups import PatternLookup
 
 from . import Geoposition
 from .forms import GeopositionField as GeopositionFormField
+from .geohash import geo_expand
 
 
 class GeopositionField(models.Field):
@@ -30,20 +32,29 @@ class GeopositionField(models.Field):
         # default case is string
         value_parts = value.rsplit(',')
         try:
-            latitude = value_parts[0]
+            geohash = value_parts[0]
+        except IndexError:
+            geohash = None
+        try:
+            latitude = value_parts[1]
         except IndexError:
             latitude = '0.0'
         try:
-            longitude = value_parts[1]
+            longitude = value_parts[2]
         except IndexError:
             longitude = '0.0'
 
-        return Geoposition(latitude, longitude)
+        return Geoposition(latitude, longitude, geohash)
 
     def from_db_value(self, value, expression, connection, context):
         return self.to_python(value)
 
     def get_prep_value(self, value):
+        if isinstance(value, Geoposition):
+            value.rehash()
+            return ",".join(
+                [value.geohash, str(value.latitude), str(value.longitude)]
+            )
         return str(value)
 
     def value_to_string(self, obj):
@@ -56,3 +67,49 @@ class GeopositionField(models.Field):
         }
         defaults.update(kwargs)
         return super(GeopositionField, self).formfield(**defaults)
+
+
+@GeopositionField.register_lookup
+class GeoSearchMatchedLookup(Lookup):
+    lookup_name = 'geosearch'
+
+    def __init__(self, lhs, rhs):
+        super(GeoSearchMatchedLookup, self).__init__(lhs, rhs)
+
+    def process_rhs(self, compiler, connection):
+        lhs, lhs_params = self.process_lhs(compiler, connection)
+        rhs, rhs_params = super(GeoSearchMatchedLookup, self).process_rhs(compiler, connection)
+        rhs = ''
+        params = rhs_params[0]
+        if params and not self.bilateral_transforms:
+            rhs_params = geo_expand(params)
+            for i in range(0, len(rhs_params)):
+                rhs_params[i] = "%s%%" % connection.ops.prep_for_like_query(rhs_params[i])
+                if i < len(rhs_params) - 1:
+                    rhs += lhs + ' like %s OR '
+                else:
+                    rhs += lhs + 'like %s'
+        return rhs, rhs_params
+
+    def as_sql(self, compiler, connection):
+        lhs, lhs_params = self.process_lhs(compiler, connection)
+        rhs, rhs_params = self.process_rhs(compiler, connection)
+        params = lhs_params + rhs_params
+        return rhs, params
+
+
+@GeopositionField.register_lookup
+class GeoPreciseSearchMatchedLookup(PatternLookup):
+    lookup_name = 'geoprecise'
+
+    def __init__(self, lhs, rhs):
+        super(GeoPreciseSearchMatchedLookup, self).__init__(lhs, rhs)
+
+    def get_rhs_op(self, connection, rhs):
+        return connection.operators['startswith'] % rhs
+
+    def process_rhs(self, qn, connection):
+        rhs, params = super(GeoPreciseSearchMatchedLookup, self).process_rhs(qn, connection)
+        if params and not self.bilateral_transforms:
+            params[0] = "%s%%" % connection.ops.prep_for_like_query(params[0])
+        return rhs, params
