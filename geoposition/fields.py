b5a5929c1e760e467e9558645cd901a3304430ec
EagerBeager <yarovoy@excentrics.ru>
2020-08-28 17:01:16 +0300

    add comments

--- geoposition/fields.py
+++ geoposition/fields.py
@@ -84,6 +84,18 @@ class GeoSearchMatchedLookup(Lookup):
     pos = Point.objects.get(id=1)
     points_matched = Point.objects.filter(position__geosearch=pos.position.geohash[0:4])
 
+    Digits and precision in km
+
+    geohash length  lat bits    lng bits	lat error	lng error	km error
+    1	            2	        3	        ±23	        ±23	        ±2500
+    2	            5	        5	        ±2.8	    ±5.6	    ±630
+    3	            7	        8	        ±0.70	    ±0.70	    ±78
+    4	            10	        10	        ±0.087	    ±0.18	    ±20
+    5	            12	        13	        ±0.022	    ±0.022	    ±2.4
+    6	            15	        15	        ±0.0027	    ±0.0055	    ±0.61
+    7	            17	        18	        ±0.00068	±0.00068	±0.076
+    8	            20	        20	        ±0.000085	±0.00017	±0.019
+
     If you want to limit the distance strictly, you should writer your own codes to filter the result.
     """
     lookup_name = 'geosearch'
